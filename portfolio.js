2d17a2bf797330ca8d5d86a404754d8cb9e46121
Jagadeesh <jagadeesh.thaniparthi@gmail.com>
2025-03-19 11:41:13 +0530

    Initial commit - Portfolio website

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+let type = new Typed("#changing-text", {
+  strings: [
+    "Web Developer",
+    "Mern Full Stack",
+    "Java Developer",
+    "React Expert",
+    "Power Bi Expert",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
---
+++ view cv.js
@@ -0,0 +1,5 @@
+function welcome() {
+  window.open(
+    "https://drive.google.com/file/d/1008MCfDuEB3USBGQwHEcEgl6b6I2D8Yh/view?usp=drivesdk"
+  );
+}
